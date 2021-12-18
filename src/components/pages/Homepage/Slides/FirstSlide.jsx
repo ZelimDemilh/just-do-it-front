00a42391744bf5eef4f4361cc7f608e1c51f908d
Зелимхан Демильханов <zelemhanzzz@mail.ru>
2021-12-18 13:41:23 +0300

    исправлено страница HomePage

---
+++ src/components/pages/Homepage/Slides/FirstSlide.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import img1 from "../img1.png";
+import cl from "../HomePage.module.css";
+
+const FirstSlide = () => {
+    return (
+        <div className="w-75 m-auto d-flex align-items-center justify-content-center justify-content-md-between py-3">
+            <img src={img1} alt="" className={`${cl.img}`}/>
+        </div>
+    );
+};
+
+export default FirstSlide;
