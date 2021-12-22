87ea65746d54a0bf1f6e0cfbb42f6e059a2fcd77
Satuev <alitm.s@mail.ru>
2021-12-22 16:41:25 +0300

    добавил вывод объявлений Заказчика и удаление объявлений

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { profile } from "../../store/signInSlice"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const Profile = () => {
   const token = useSelector((state) => state.signIn.token)
@@ -55,7 +55,14 @@ const Profile = () => {
                     <td>Рейтинг:</td>
                     <td>{userDate.rating}</td>
                   </tr>
-                ) : null}
+                ) : (
+                  <Link
+                    to="/my-tasks"
+                    className="btn btn-danger w-100 p-1 my-2"
+                  >
+                    Мои объявления
+                  </Link>
+                )}
               </tbody>
             </table>
 
--- src/store/taskSlice.jsx
+++ src/store/taskSlice.jsx
@@ -1,50 +1,75 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 export const addTaskForm = createAsyncThunk(
-    "taskForm/addTaskForm",
-    async function ({
-                      heading: heading,
-                      description: description,
-                      price: price,
-                      category: category,
-                      latitude: latitude,
-                      longitude: longitude
-                    }) {
-      try {
-        const res = await fetch("http://localhost:6557/tasks/add", {
-          method: "POST",
-          body: JSON.stringify({
-            header: heading,
-            description: description,
-            price: price,
-            category: category,
-            latitude: latitude,
-            longitude: longitude
-          }),
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`
-          },
-        });
-        const data = await res.json();
+  "taskForm/addTaskForm",
+  async function ({
+    heading: heading,
+    description: description,
+    price: price,
+    category: category,
+    latitude: latitude,
+    longitude: longitude,
+  }) {
+    try {
+      const res = await fetch("http://localhost:6557/tasks/add", {
+        method: "POST",
+        body: JSON.stringify({
+          header: heading,
+          description: description,
+          price: price,
+          category: category,
+          latitude: latitude,
+          longitude: longitude,
+        }),
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      const data = await res.json()
 
-        return data;
-      } catch (e) {
-        console.log(e);
-      }
+      return data
+    } catch (e) {
+      console.log(e)
     }
-);
+  }
+)
+
+export const deleteTask = createAsyncThunk(
+  "tasks/deleteTask",
+  async function (id, { rejectWithValue }) {
+    try {
+      const res = await fetch(`http://localhost:6557/tasks//remove/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+
+      const data = await res.json()
+
+      if (data.error) {
+        throw new Error(data.error)
+      }
+
+      return id
+    } catch (error) {
+      return rejectWithValue(error.message)
+    }
+  }
+)
 
 export const uploadTasks = createAsyncThunk(
   "tasks/uploadTasks",
   async function () {
-    const res = await fetch("http://localhost:6557/tasks");
+    const res = await fetch("http://localhost:6557/tasks")
 
-    const data = await res.json();
+    const data = await res.json()
 
-    return data;
+    return data
   }
-);
+)
 
 const taskSlice = createSlice({
   name: "task",
@@ -55,39 +80,53 @@ const taskSlice = createSlice({
   },
   reducers: {
     getTasks(state, action) {
-      state.task.push(action.payload);
+      state.task.push(action.payload)
     },
   },
   extraReducers: {
     [uploadTasks.pending]: (state, action) => {
-      state.pending = true;
-      state.error = null;
+      state.pending = true
+      state.error = null
     },
     [uploadTasks.fulfilled]: (state, action) => {
-      state.pending = false;
-      state.error = null;
-      state.task = action.payload;
+      state.pending = false
+      state.error = null
+      state.task = action.payload
     },
     [uploadTasks.rejected]: (state, action) => {
-      state.pending = false;
-      state.error = action.payload.error;
+      state.pending = false
+      state.error = action.payload.error
     },
-      [addTaskForm.pending]: (state) => {
-          state.pending = true;
-      },
-      [addTaskForm.fulfilled]: (state, action) => {
-          state.pending = false;
-          state.error = null;
-          state.task = [action.payload];
-      },
-      [addTaskForm.rejected]: (state, action) => {
-          state.pending = false;
-          state.task = null;
-          state.error = action.payload;
-      },
+
+    [addTaskForm.pending]: (state) => {
+      state.pending = true
+    },
+    [addTaskForm.fulfilled]: (state, action) => {
+      state.pending = false
+      state.error = null
+      state.task = [action.payload]
+    },
+    [addTaskForm.rejected]: (state, action) => {
+      state.pending = false
+      state.task = null
+      state.error = action.payload
+    },
+
+    [deleteTask.pending]: (state) => {
+      state.pending = true
+    },
+    [deleteTask.fulfilled]: (state, action) => {
+      state.pending = false
+      state.error = null
+      state.task = state.task.filter((task) => task._id !== action.payload)
+    },
+    [deleteTask.rejected]: (state, action) => {
+      state.pending = false
+      state.error = action.payload
+    },
   },
-});
+})
 
-export const { getTasks } = taskSlice.actions;
+export const { getTasks } = taskSlice.actions
 
-export default taskSlice.reducer;
+export default taskSlice.reducer
